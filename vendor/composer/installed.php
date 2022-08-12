7ffc8cb0314c46c69c590aa292a175417f23354a
ihsbramn <abdurahmanihsan25@gmail.com>
2022-08-12 22:16:51 +0700

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7b1229e497077a493eeb923e67d518eaccae8833',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7b1229e497077a493eeb923e67d518eaccae8833',
+            'dev_requirement' => false,
+        ),
+    ),
+);
